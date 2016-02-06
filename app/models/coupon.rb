90acb72b3243fc4ae173c4b198c1beb87a913e5b
bogdan.popa <bogdan.popa.a@gmail.com>
2016-02-06 16:39:17 +0200

    Generate coupon code after order

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,5 +3,10 @@ class Coupon < ActiveRecord::Base
   belongs_to :variant
 
   validates :code, presence: true
-  validates :code, :format => { :with => /\A(?=.*[a-z])[a-z\d]+\Z/i }
+# validates :code, :format => { :with => /\A(?=.*[a-z])[a-z\d]+\Z/i }
+
+  def self.generate_code(variant)
+    new_coupon = self.new(code: SecureRandom.urlsafe_base64, variant_id: variant.id)
+    new_coupon.save!
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -6,9 +6,14 @@ class Order < ActiveRecord::Base
 
   def substract_credits
     credits = self.buyer.credits
-    product_price = self.product.variants.where(:is_active => true).map(&:price).min
+    product_variants = self.product.variants.where(:is_active => true)
+    product_price = product_variants.map(&:price).min
     credits_remaining = credits - product_price
+
     self.buyer.credits = credits_remaining
     self.buyer.save!
+
+    Coupon.generate_code(product_variants.order(:price).first)
   end
+
 end
