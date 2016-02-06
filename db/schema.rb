c26078c62755fbcb1ea6eb7d0ce2da37103948b8
bogdan.popa <bogdan.popa.a@gmail.com>
2016-02-06 14:56:36 +0200

    Add variant to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206115434) do
+ActiveRecord::Schema.define(version: 20160206125518) do
 
   create_table "buyers", force: :cascade do |t|
     t.string   "email",                  default: "",  null: false
@@ -47,7 +47,10 @@ ActiveRecord::Schema.define(version: 20160206115434) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "variant_id"
   end
+
+  add_index "products", ["variant_id"], name: "index_products_on_variant_id"
 
   create_table "variants", force: :cascade do |t|
     t.boolean  "is_active",  default: true
