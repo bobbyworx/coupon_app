2deed49b08e99a095343cd8d0a0ac7d6b16139a1
bogdan.popa <bogdan.popa.a@gmail.com>
2016-02-06 14:43:24 +0200

    Variant seeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
+  has_many :variants
+
   validates :title, presence: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Variant.create(is_active: 'true', price: 123, quantity: 4)
+Variant.create(is_active: 'true', price: 223, quantity: 6)
+Variant.create(is_active: 'true', price: 222, quantity: 4)
+Variant.create(is_active: 'true', price: 411, quantity: 1)
+Variant.create(is_active: 'true', price: 510, quantity: 2)
+Variant.create(is_active: 'true', price: 400, quantity: 8)
+Variant.create(is_active: 'true', price: 800, quantity: 0)
+p "Variants created!"
